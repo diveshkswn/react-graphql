9c593179d188daba255980c021be4de6facaea45
Divesh Keswani <diveshkswn@gmail.com>
2022-02-12 23:09:59 +0530

    apollo provider

--- components/CharacterList/CharactersList.tsx
+++ components/CharacterList/CharactersList.tsx
@@ -1,11 +1,32 @@
 import { useQuery } from '@apollo/client';
-import { CharactersList } from './CharactersList.types';
-import {} from './CharactersList.graphql';
+import Image from 'next/image';
+import { CharactersListProps } from './CharactersList.types';
+import { GET_ALL_CHARACTERS } from './CharactersList.graphql';
+import styles from '../../styles/CharactersList.module.css';
 
-function CharactersList(props) {
-  const list = useQuery();
+const renderCharacters = (name : string, imgUrl : string, index:number) => (
+  <div className={styles.CharacterCard} key={index}>
+    <h3 className={styles.characterName}>{name}</h3>
+    <div className={styles.imgContainer}>
+      <Image src={imgUrl} alt={name} layout="fill" />
+    </div>
+  </div>
+);
+
+function CharactersList(props : CharactersListProps) {
+  const { error, data, loading } = useQuery(GET_ALL_CHARACTERS);
+
+  if (loading) { return <h3>loading....</h3>; }
+
   return (
-    <h1>Characters</h1>
+    <>
+      <h1 className={styles.heading}>Characters</h1>
+      <div className={styles.characterList}>
+        {data.characters.results.map((item :{name : string, image : string}, index : number) => (
+          renderCharacters(item.name, item.image, index)
+        ))}
+      </div>
+    </>
   );
 }
 
